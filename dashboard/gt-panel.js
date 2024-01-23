b46dba677285abb43f3919e69262aec04e7298bb
fhtklro <3281185@coe.ntt.com>
2024-01-23 16:38:33 +0900

    wip

--- dashboard/gt-panel.js
+++ dashboard/gt-panel.js
@@ -1,9 +1,16 @@
-const docInputFile = document.getElementById("input_file")
-const docSelectTelop = document.getElementById("select_telop")
+const docInputFile = document.getElementById("input_file");
+const docSelectTelop = document.getElementById("select_telop");
+const docTelopName = document.getElementById("telop_name");
+const docTelopDescription = document.getElementById("telop_description");
+const docButtonReset = document.getElementById("reset")
+const docButtonSubmit = document.getElementById("submit")
 
-let inputData = ""
-let csvData = []
+// const repValue = nodecg.replicant("repValue")
 
+let inputData = "";
+let csvData = [];
+
+// ファイルのアップロード
 docInputFile.addEventListener('change', (e) => {
     const reader = new FileReader();
     reader.readAsText(e.target.files[0]);
@@ -11,16 +18,28 @@ docInputFile.addEventListener('change', (e) => {
     reader.onload = function(event) {
         inputData = event.target.result;
         input2Csv(inputData);
-        // console.log("debug "+csvData)
         genSelectBox(csvData);
-
+        selectDefault();
     };
 });
 
+// csvファイルの読み込み
+function input2Csv(text) {
+    const inputData = text.replace(/\r/g, '').split('\n');
+    const lineCount = inputData.length;
+
+    for(let i=0; i<lineCount; i++) {
+        const pushdata = inputData[i].split(",");
+        
+        if(pushdata.length == 2) {
+            csvData.push(pushdata);
+        }
+    }
+}
+
+// セレクトボックスへの反映
 function genSelectBox(cData) {
     let count = cData.length;
-
-    console.log(cData)
 
     for(let i=0; i<count; i++) {
         const option = document.createElement('option');
@@ -30,20 +49,26 @@ function genSelectBox(cData) {
     }
 }
 
-function input2Csv(text) {
-    const inputData = text.split('\n');
-    const lineCount = inputData.length;
+// セレクトボックスの初期化
+function selectDefault() {
+    docSelectTelop.selectedIndex = -1;
+}
 
-    // console.log(inputData)
+// テキストボックスに反映
+docSelectTelop.addEventListener('change', (e) => {
+    const selectValue = e.target.value;
+    console.log(selectValue, csvData[selectValue]);
+    docTelopName.value = csvData[selectValue][0];
+    docTelopDescription.innerHTML = csvData[selectValue][1];
+});
 
-    for(let i=0; i<lineCount; i++) {
-        let pushdata = inputData[i].split(",")
-        
-        if(pushdata.length == 2) {
-            // console.log(pushdata)
-            csvData.push(pushdata)
-        }
-        // console.log("---")
-    }
-    // console.log(csvData)
+// リセット
+docButtonReset.onclick = () => {
+    docTelopName.value = "";
+    docTelopDescription.innerHTML = "";
+}
+
+// 反映
+docButtonSubmit.onclick = () => { 
+    // repValue = [docTelopName.value, docTelopDescription.innerHTML];
 }
